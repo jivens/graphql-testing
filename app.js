4a8e2df90a50457cf9f0081ca9823177f58a4452
John Ivens <john.wagner.ivens@gmail.com>
2019-05-17 08:26:18 -0700

    Added sequelize to graphql server

--- app.js
+++ app.js
@@ -1,8 +1,54 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+// store config variables in dotenv
+require('dotenv').config();
+// ORM (Object-Relational Mapper library)
+const Sequelize = require('sequelize');
 
 const app = express();
+
+// ****** Set up default MYSQL connection START ****** //
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    //operatorsAliases: false,
+    pool: { max: 5, min: 0, acquire: 30000, idle: 10000 },
+    define: {
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_unicode_ci',
+      timestamps: true
+    },
+    //logging:false
+  });
+
+sequelize
+.authenticate()
+.then(() => {
+  console.log('connected to MYSQL- COLRC database');
+})
+.catch(err => {
+  console.error('Unable to connect to the database:', err);
+});
+// ****** Set up default MYSQL connection END ****** //
+
+//
+// User model for sequelize
+//
+const User = sequelize.define('user', {
+  name: { type: Sequelize.STRING },
+  email: { type: Sequelize.STRING },
+  password: { type: Sequelize.STRING },
+  roles: { type: Sequelize.STRING }
+},
+{
+  charset: 'utf8mb4',
+  collate: 'utf8mb4_unicode_ci'
+});
 
 app.use("/graphql", graphqlHTTP({
   schema,
